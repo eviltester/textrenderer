b09c034528706d644d1190a7274ebf10567bda11
alan <alan@compendiumdev.co.uk>
2019-10-18 10:06:34 +0100

    initial split of renderer from the web page to make it a component

--- js/renderer.js
+++ js/renderer.js
@@ -1,5 +1,100 @@
 // https://stackoverflow.com/questions/2936112/text-wrap-in-a-canvas-element
 
+const rendering_gui_html = `
+    <div id="rendering">
+
+        <button onclick="renderImages()">RENDER</button>
+        <br/>
+
+        <div>
+            <div class="canvasdisplay" style="width:47%;float:left;margin-right:1em;">
+                <canvas id="renderslogan" width="1080px" height="1080px" style="width:100%"/>
+            </div>
+            <div id="renderingcontrols" style="width:47%; float:right">
+                <select id="font" onchange="renderImages()">
+                    <!-- https://www.w3schools.com/cssref/css_websafe_fonts.asp -->
+                    <option value="serif">serif</option>
+                    <option value="georgia">Georgia</option>
+                    <option value="palatino">Palatino</option>
+                    <option value="palatino linotype">Palatino Linotype</option>
+                    <option value="book antiqua">Book Antiqua</option>
+                    <option value="times new roman">Times New Roman</option>
+                    <option value="times">Times</option>
+
+                    <option value="sans serif">---</option>
+                    <option value="sans serif">sans serif</option>
+                    <option value="arial">Arial</option>
+                    <option value="helvetica">Helvetica</option>
+                    <option value="arial black">Arial Black</option>
+                    <option value="gadget">Gadget</option>
+                    <option value="comic sans ms">Comic Sans MS</option>
+                    <option value="cursive">Cursive</option>
+                    <option value="impact">Impact</option>
+                    <option value="charcoal">Charcoal</option>
+                    <option value="lucida sans unicode">Lucida Sans Unicode</option>
+                    <option value="lucida grande">Lucida Grande</option>
+                    <option value="tahoma">Tahoma</option>
+                    <option value="geneva">Geneva</option>
+                    <option value="trebuchet ms">Trebuchet MS</option>
+                    <option value="verdana">Verdana</option>
+
+                    <option value="monospace">---</option>
+                    <option value="monospace">monospace</option>
+                    <option value="courier new">Courier New</option>
+                    <option value="courier">Courier</option>
+                    <option value="monaco">Monaco</option>
+                    <option value="lucida console">Lucida Console</option>
+
+                    <option value="roboto">---</option>
+                    <option value="roboto">Roboto</option>
+                    <option value="garamond">Garamond</option>
+                    <option value="bookman">Bookman</option>
+                    <option value="candara">Candara</option>
+                    <option value="calibri">Calibri</option>
+
+                </select>
+                <br/>
+
+                <label for="fontsize">Font Size</label>
+                <input type="range" min="1" max="200" value="15" class="slider" style="width:50%" id="fontsize" onchange="
+            document.getElementById('fontsizedisplay').value = document.getElementById('fontsize').value;document.getElementById('autofontsize').checked=false; renderImages()">
+                <input id="fontsizedisplay" value="15"></input>
+                <br/>
+                <input type="checkbox" id="autofontsize" checked>Auto Size
+                <hr/>
+
+
+
+                <br/>
+                <label for="maxcharsperline">Max Chars Per Line</label>
+                <input type="range" min="1" max="50" value="15" class="slider" style="width:80%" id="maxcharsperline" onchange="
+            document.getElementById('maxcharsperlinedisplay').value = document.getElementById('maxcharsperline').value; renderImages()">
+                <input id="maxcharsperlinedisplay" value="15"></input>
+                <hr/>
+
+                <br/>
+                <label for="backcolorpicker">BackGround Colour</label>
+                <input type="color" id="backcolorpicker" value="#ff0000" onchange="renderImages()" style="width:50%;">
+                <br/>
+                <label for="textcolorpicker">Text Colour</label>
+                <input type="color" id="textcolorpicker" value="#000000"  onchange="renderImages()"  style="width:50%;">
+                <br/>
+
+                <p>for .jpg right click and save as below:</p>
+                <img id="renderjpg" width="50px" height="50px"/>
+                <br/>
+
+            </div>
+        </div>
+    </div>
+`;
+
+function displayIn(anId){
+    if(!document.getElementById("rendering")) {
+        document.getElementById(anId).insertAdjacentHTML('beforeend', rendering_gui_html);
+    }
+}
+
 function wrapText(ctx, text, maxWidth, lineHeight) {
     var lines=[];
     var words = text.split(' ');
