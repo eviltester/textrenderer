26a1f8a2f7e3bf7759441508efc27aa2cfbd7c4a
alan <alan@compendiumdev.co.uk>
2019-10-31 14:36:13 +0000

    added text comment

--- js/renderer.js
+++ js/renderer.js
@@ -1087,10 +1087,14 @@ function Renderer() {
 
 
 
-// TODO: allow a background image and an opacity for the background colour - need to catch error and make readable error for tainted image and this code needs to be programmatically activated as an advanced mode (tried copying into intermediate canvas but that didn't work)
+// TODO: allow a background image and an opacity for the background colour - need to catch error and make readable error for tainted image and this code needs to be programmatically activated as an advanced mode (tried copying into intermediate canvas but that didn't work) - workaround add links to png to jpg online conversion services
 // TODO: when image and opacity is available allow 'margin' for the background colour to adjust amount of background image shown
 // TODO: can we pull in list of font names supported by browser rather than hard code?
 // TODO: make javascript control html template more configurable and code generated
+// TODO: add a background shape e.g square, circle, rectangle - config colour, x, y, width, height and rotation
+// TODO: add linear gradiant background, background shape
+// TODO: add radial gradiant background, background shape
+
 
 
 }
